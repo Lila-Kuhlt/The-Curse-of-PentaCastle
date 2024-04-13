252a9aece25a287e8a201ff443981014f79076f4
konsumlamm <konsumlamm@gmail.com>
2024-04-13 15:57:51 +0200

    Start basic door room loading

--- src/scripts/world.gd
+++ src/scripts/world.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var rooms = ["res://scenes/world.tscn"]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,3 +10,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func new_room():
+	var room = rooms.pick_random()
+	get_tree().change_scene_to_file(room)
+
+
+func _on_door_body_entered(_body):
+	new_room()
