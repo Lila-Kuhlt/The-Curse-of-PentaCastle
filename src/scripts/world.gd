da31d3e1bcb79e5ee2926c44cca78d0e1e48925e
Robin Ebert <ebertrobin2002@gmail.com>
2024-04-13 18:14:05 +0200

    Fix debugger error

--- src/scripts/world.gd
+++ src/scripts/world.gd
@@ -4,7 +4,7 @@ var rooms = ["res://scenes/room.tscn"]
 
 func new_room():
 	var room = rooms.pick_random()
-	get_tree().change_scene_to_file(room)
+	get_tree().call_deferred("change_scene_to_file", room)
 
 func _on_door_body_entered(_body):
 	new_room()
