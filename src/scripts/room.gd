9dd755eba5fb08d8dc5937bfc416a5b23b0f4178
konsumlamm <konsumlamm@gmail.com>
2024-04-13 18:35:39 +0200

    Implement chest spawning

--- src/scripts/room.gd
+++ src/scripts/room.gd
@@ -1,14 +1,37 @@
 extends Node2D
 
+@export var CHESTS := 1
+@export var MONSTERS := 1
+
+const Chest = preload("res://scenes/chest.tscn")
+
 func _ready():
-	var cells = $TileMap.get_used_cells(0)
+	var cells: Array[Vector2i] = $TileMap.get_used_cells(0)
+	var chests: Array[Vector2i] = []
+	var monsters: Array[Vector2i] = []
 	for cell in cells:
 		match $TileMap.get_cell_tile_data(0, cell).get_custom_data("name"):
 			"chest":
-				$TileMap.set_cell(0, cell)
+				chests.append(cell)
 			"spawner":
 				$TileMap.set_cell(0, cell)
+				monsters.append(cell)
+			_:
+				continue
+		$TileMap.set_cell(0, cell)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	# chest spawning
+	chests.shuffle()
+	chests.slice(0, CHESTS)
+	for cell in chests:
+		var chest := Chest.instantiate()
+		chest.position = $TileMap.map_to_local(cell)
+		add_child(chest)
+
+	# monster spawning
+	monsters.shuffle()
+	monsters.slice(0, MONSTERS)
+	# TODO
+
 func _process(_delta):
 	pass
