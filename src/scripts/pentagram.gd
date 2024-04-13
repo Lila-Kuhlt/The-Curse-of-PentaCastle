39e63cc42126cd463b4ee68c6147bab6dc0155eb
konsumlamm <konsumlamm@gmail.com>
2024-04-13 14:50:30 +0200

    Add janky controller support for summoning

--- src/scripts/pentagram.gd
+++ src/scripts/pentagram.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 const CLIP_DISTANCE := 90.0
+const CLIP_ANGLE := 0.5
 
 @onready var image: TextureRect = $Pentagram
 @onready var multiline: Line2D = $Multiline
@@ -22,16 +23,18 @@ var line_length := 0.0
 
 func activate_combo() -> void:
 	print('combo: ', combo)
+	# reset combo
+	combo = []
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		image.position = event.position - image.size * image.scale * 0.5
+	if event.is_action("summon"):
+		var pos = event.position if "position" in event else get_viewport().get_visible_rect().size / 2
+		image.position = pos - image.size * image.scale * 0.5
 		is_drawing = event.is_pressed()
 		if not is_drawing:
 			multiline.clear_points()
 			if combo:
 				activate_combo()
-			combo = []
 		image.visible = is_drawing
 	if event is InputEventMouseMotion and is_drawing:
 		var pos: Vector2 = event.position
@@ -55,3 +58,20 @@ func _input(event: InputEvent) -> void:
 			line_length += pos.distance_to(multiline.points[multiline.points.size() - 1])
 		multiline.add_point(pos)
 		multiline.material.set_shader_parameter('line_length', line_length)
+	if event is InputEventJoypadMotion and is_drawing:
+		var direction := Input.get_vector("summon_left", "summon_right", "summon_up", "summon_down")
+		if not direction: return
+		var angle := direction.angle()
+		for i in PENTAGRAM_CORNERS.size():
+			if combo and combo[-1] == i: continue
+			var corner_angle := (PENTAGRAM_CORNERS[i] - Vector2(0.5, 0.5)).angle()
+			if abs(angle_difference(angle, corner_angle)) <= CLIP_ANGLE:
+				combo.append(i)
+				var pos := image.position + image.size * image.scale * PENTAGRAM_CORNERS[i]
+				var effect = clip_effect.instantiate()
+				effect.position = pos
+				effect.emitting = true
+				add_child(effect)
+				multiline.add_point(pos)
+				multiline.material.set_shader_parameter('line_length', line_length)
+				break
