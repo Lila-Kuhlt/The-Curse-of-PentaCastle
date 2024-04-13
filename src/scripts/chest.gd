2a2b8d57e460cb430e93ba6147ab5ec64535b3aa
konsumlamm <konsumlamm@gmail.com>
2024-04-13 15:11:24 +0200

    Use collision layer 2 for chest detection

--- src/scripts/chest.gd
+++ src/scripts/chest.gd
@@ -18,17 +18,15 @@ func use_chest():
 
 
 func _on_body_entered(body):
-	if (body is CharacterBody2D):
-		is_player_entered = true
-		show_use_label()
-		player_entered = body
+	is_player_entered = true
+	show_use_label()
+	player_entered = body
 
 
-func _on_body_exited(body):
-	if (body is CharacterBody2D):
-		is_player_entered = false
-		hide_use_label()
-		player_entered = null
+func _on_body_exited(_body):
+	is_player_entered = false
+	hide_use_label()
+	player_entered = null
 
 func show_use_label():
 	use_label.visible = true
