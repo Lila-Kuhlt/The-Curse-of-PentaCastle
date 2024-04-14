3bc791eb44ee39601613800428555db1c019314d
nat-rix <upezu@student.kit.edu>
2024-04-14 12:03:07 +0200

    is_player_right != is player right ihr Flaschen

--- src/scripts/spells/fireball.gd
+++ src/scripts/spells/fireball.gd
@@ -3,12 +3,12 @@ extends SpellBase
 func cast(player: CharacterBody2D, _enemies: Array[Node]):
 	var Projectile = load("res://scenes/projectiles/fireball.tscn")
 	var projectile = Projectile.instantiate()
-	if (player.is_turned_right):
-		projectile.direction = Vector2(1, 0)
-		projectile.rotation = 0
-	else:
+	if  player.sprite.flip_h:
 		projectile.direction = Vector2(-1, 0)
 		projectile.rotation = PI
+	else:
+		projectile.direction = Vector2(1, 0)
+		projectile.rotation = 0
 
 	projectile.position = player.position
 	player.get_parent().add_child(projectile)
