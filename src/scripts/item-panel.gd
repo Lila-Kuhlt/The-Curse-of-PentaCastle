3be337432259c0fee7e212547db69ea9fc759353
nat-rix <upezu@student.kit.edu>
2024-04-14 03:30:58 +0200

    Kranich
    Kranich mal endlich sterben gehen?

---
+++ src/scripts/item-panel.gd
@@ -0,0 +1,29 @@
+extends Panel
+
+@onready var bg: Line2D = $Line2DBG
+@onready var fg: Line2D = $Line2DFG
+
+const SCALE := 25.0
+const OFFSET := Vector2(-4.0, -4.0)
+
+const PENTAGRAM_CORNERS: Array[Vector2] = [
+	Vector2(0.5, 0.885),
+	Vector2(0.134, 0.619),
+	Vector2(0.273, 0.188),
+	Vector2(0.727, 0.188),
+	Vector2(0.866, 0.619),
+]
+
+func init_lines(line: Array[int]):
+	for i in [0, 2, 4, 1, 3, 0, 1, 2, 3, 4, 0]:
+		bg.add_point(PENTAGRAM_CORNERS[i] * SCALE + OFFSET)
+	for i in line:
+		fg.add_point(PENTAGRAM_CORNERS[i] * SCALE + OFFSET)
+
+func _on_mouse_entered() -> void:
+	fg.visible = true
+	bg.visible = true
+
+func _on_mouse_exited() -> void:
+	fg.visible = false
+	bg.visible = false
--- src/scripts/player_ui.gd
+++ src/scripts/player_ui.gd
@@ -1,35 +1,47 @@
 extends CanvasLayer
 
 @onready var spellsBox = $SpellsHBox
+@onready var inPanel = $SpellsHBox/MarginContainer
+
+var dflt_box := StyleBoxFlat.new()
+var ovrride_box := StyleBoxFlat.new()
+
+func _ready() -> void:
+	dflt_box.bg_color = Color(0.3, 0.3, 0.3)
+	ovrride_box.bg_color = Color(0.4, 0.5, 0.4)
+	for box in [dflt_box, ovrride_box]:
+		box.set_border_width_all(2.0)
+		box.border_color = Color(0.2, 0.15, 0.2)
 
 func add_spell_item_panel(spell: SpellBook.Spells):
-	var new_item_panel = Panel.new()
-	new_item_panel.custom_minimum_size.x = 12
-	spellsBox.add_child(new_item_panel)
-	var spell_sprite = Sprite2D.new()
-	spell_sprite.texture = SpellBook.spell_item_sprites[spell]
-	spell_sprite.position += Vector2(6, 6)
-	new_item_panel.add_child(spell_sprite)
+	var panel: MarginContainer = inPanel.duplicate()
+	panel.visible = true
+	spellsBox.add_child(panel)
+	var lst: Array[int] = []
+	for c in SpellBook.spell_item_scripts[spell].new().cast:
+		lst.append(int(c))
+	var child: Panel = panel.get_child(0)
+	child.init_lines(lst)
+	child.add_theme_stylebox_override('panel', dflt_box)
+	_set_sprite_texture(child.get_child(0), spell)
 
 func mark_spell_item_panel(idx: int):
-	var item_panel: Panel = spellsBox.get_child(idx)
-	var new_stylebox = StyleBoxFlat.new()
-	new_stylebox.bg_color = Color(1,1,1)
-	item_panel.add_theme_stylebox_override("panel", new_stylebox)
+	var panel: Panel = spellsBox.get_child(idx + 1).get_child(0)
+	panel.remove_theme_stylebox_override('panel')
+	panel.add_theme_stylebox_override('panel', ovrride_box)
 
 func unmark_spell_item_panel(idx: int):
-	var item_panel: Panel = spellsBox.get_child(idx)
-	item_panel.remove_theme_stylebox_override("panel")
+	var panel: Panel = spellsBox.get_child(idx + 1).get_child(0)
+	panel.remove_theme_stylebox_override('panel')
+	panel.add_theme_stylebox_override('panel', dflt_box)
 
 func replace_spell_item_panel(idx: int, new_spell: SpellBook.Spells):
-	var item_panel = spellsBox.get_child(idx)
-	var spell_sprite = item_panel.get_child(0)
-	_set_sprite_texture(spell_sprite, new_spell)
+	var panel: Panel = spellsBox.get_child(idx + 1).get_child(0)
+	_set_sprite_texture(panel.get_child(0), new_spell)
 
 func delete_spell_item_panel(idx: int):
-	var item_panel = spellsBox.get_child(idx)
+	var item_panel = spellsBox.get_child(idx + 1)
 	item_panel.queue_free()
 
-func _set_sprite_texture(spell_sprite: Sprite2D, spell: SpellBook.Spells):
+func _set_sprite_texture(spell_sprite: TextureRect, spell: SpellBook.Spells):
 	spell_sprite.texture = SpellBook.spell_item_sprites[spell]
-	spell_sprite.position += Vector2(6, 6)
