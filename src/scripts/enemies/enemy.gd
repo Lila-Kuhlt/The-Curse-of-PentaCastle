ab45a20d341a690953debeaa6d75bf0f7fc28d5b
EliasSchaut <eschaut@outlook.de>
2024-04-14 02:27:54 +0200

    projectiles make damage and knockback now

--- src/scripts/enemies/enemy.gd
+++ src/scripts/enemies/enemy.gd
@@ -4,21 +4,24 @@ class_name Enemy extends CharacterBody2D
 @export var MOVEMENT_SPEED := 10.0
 @export var ATTACK_DAMAGE := 10.0
 @export var KNOCKBACK_STRENGTH := 40.0
+@export var KNOCKBACK_ENVELOPE: float = 0.86
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var knockback = Vector2(0, 0)
 
 func do_physics(delta):
+	if (LIFE <= 0):
+		i_am_gonna_kill_myself()
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	velocity += knockback
+	knockback *= KNOCKBACK_ENVELOPE
+
 
 func hit_player(player: CharacterBody2D):
 	player.LIFE -= ATTACK_DAMAGE
 	var direction = (player.velocity * -1).normalized()
 	player.knockback = direction * KNOCKBACK_STRENGTH
 	print(player.LIFE)
-
-func _process(delta):
-	if (LIFE <= 0):
-		i_am_gonna_kill_myself()
 
 func i_am_gonna_kill_myself():
 	queue_free()
--- src/scripts/projectile.gd
+++ src/scripts/projectile.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 @export var speed := 60.0
-
+@export var ATTACK_DAMAGE := 10.0
+@export var KNOCKBACK_STRENGTH := 30.0
 var direction: Vector2 = Vector2(1, 0)
 
 func _physics_process(delta: float):
@@ -9,4 +10,9 @@ func _physics_process(delta: float):
 	var collision := move_and_collide(velocity * delta)
 	if collision:
 		print(collision.get_collider())
+		hit_body(collision.get_collider())
 		queue_free()
+
+func hit_body(body: CharacterBody2D):
+	body.LIFE -= ATTACK_DAMAGE
+	body.knockback = direction.normalized() * KNOCKBACK_STRENGTH
