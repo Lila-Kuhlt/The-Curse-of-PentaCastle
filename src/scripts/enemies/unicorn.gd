12235d66b06308dcccd59c2f2bf7fb8deb961907
konsumlamm <konsumlamm@gmail.com>
2024-04-15 23:45:51 +0200

    Fix stuff

--- src/scripts/enemies/unicorn.gd
+++ src/scripts/enemies/unicorn.gd
@@ -2,11 +2,11 @@ extends Enemy
 
 enum UnicornMode { CHARGE, CHARGE_START, STAND, WALK }
 
-const MIN_WALK_COOLDOWN := 100
-const MAX_WALK_COOLDOWN := 300
-const MIN_STAND_COOLDOWN := 30
-const MAX_STAND_COOLDOWN := 200
-const CHARGE_COOLDOWN := 50
+const MIN_WALK_COOLDOWN := 1.0
+const MAX_WALK_COOLDOWN := 5.0
+const MIN_STAND_COOLDOWN := 0.5
+const MAX_STAND_COOLDOWN := 4.0
+const CHARGE_COOLDOWN := 1.0
 const WALK_MULT := 0.6
 const CHARGE_MULT := 7.0
 const VIEW_RAY_WIDTH := 10.0
@@ -15,25 +15,25 @@ const VIEW_RAY_WIDTH := 10.0
 @onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
 
 var mode := UnicornMode.STAND
-var mode_cooldown := 100
+var mode_cooldown := 1.0
 var was_on_wall_cooldown := 0.0
 
 func goto_stand_mode():
 	mode = UnicornMode.STAND
 	anim.play('idle')
-	mode_cooldown = randi_range(MIN_STAND_COOLDOWN, MAX_STAND_COOLDOWN)
+	mode_cooldown = randf_range(MIN_STAND_COOLDOWN, MAX_STAND_COOLDOWN)
 
 func goto_walk_mode(toggle: bool):
 	mode = UnicornMode.WALK
 	if toggle or (randi() & 1):
 		flip_direction()
 	anim.play('walk')
-	mode_cooldown = randi_range(MIN_WALK_COOLDOWN, MAX_WALK_COOLDOWN)
+	mode_cooldown = randf_range(MIN_WALK_COOLDOWN, MAX_WALK_COOLDOWN)
 
 func _physics_process(delta):
 	# player detection
 	var collider = $ViewRay.get_collider()
-	if collider == get_tree().get_first_node_in_group("player"):
+	if collider == player:
 		if mode != UnicornMode.CHARGE_START and mode != UnicornMode.CHARGE:
 			mode = UnicornMode.CHARGE_START
 			mode_cooldown = CHARGE_COOLDOWN
@@ -51,7 +51,10 @@ func _physics_process(delta):
 					goto_stand_mode()
 					flip_direction()
 			else:
+				print("cooldown: ", mode_cooldown)
+				print("subtracting ", delta)
 				mode_cooldown -= delta
+				print("new cooldown: ", mode_cooldown)
 		UnicornMode.WALK:
 			if is_on_wall():
 				is_facing_right = get_wall_normal().x > 0.0
