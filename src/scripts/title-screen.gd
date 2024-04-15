7226a32083b080dab9e255301cc22a9041d4fc70
nat-rix <upezu@student.kit.edu>
2024-04-16 00:01:19 +0200

    Title Screen

--- src/scripts/title-screen.gd
+++ src/scripts/title-screen.gd
@@ -2,3 +2,6 @@ extends Control
 
 func on_start_game_button_pressed() -> void:
 	get_tree().change_scene_to_file('res://scenes/world.tscn')
+
+func on_exit_button_pressed() -> void:
+	get_tree().quit()
