1d654851b5805c7858463e9cb694ae34212b6bd2
Robin Ebert <ebertrobin2002@gmail.com>
2024-04-15 19:20:24 +0200

    Move down spell projectile spawn point

--- src/scripts/spells/base.gd
+++ src/scripts/spells/base.gd
@@ -10,6 +10,6 @@ func spawn_projectile(player: Player, projectile: Projectile):
 	var flip: bool = player.sprite.flip_h
 	projectile.set_flipped(flip)
 	projectile.direction = Vector2(1,0) * (-1 if flip else 1)
+	projectile.position = player.position + Vector2(0, 4)
 
-	projectile.position = player.position
 	player.get_parent().add_child(projectile)
