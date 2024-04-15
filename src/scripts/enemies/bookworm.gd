9764bb8625c216e335f3130588b71061871a98fd
nat-rix <upezu@student.kit.edu>
2024-04-15 20:12:28 +0200

    Mein Niwo, Dein Nivo: ABSTAND!

--- src/scripts/enemies/bookworm.gd
+++ src/scripts/enemies/bookworm.gd
@@ -1,6 +1,7 @@
 extends Enemy
 
 const VIEW_DISTANCE := 70.0
+const MIN_DISTANCE_TO_PLAYER := 40.0
 const IDLE_COOLDOWN_MIN := 4.0
 const IDLE_COOLDOWN_MAX := 8.0
 const HIDE_COOLDOWN_MIN := 1
@@ -44,7 +45,9 @@ func goto_hide_mode():
 	anim.queue('hide')
 
 func goto_idle_mode():
-	position = find_teleport_pos()
+	var tpos := find_teleport_pos()
+	if tpos.x != INF:
+		position = tpos
 	mode = BookwormMode.IDLE
 	shape.disabled = false
 	mode_cooldown = randf_range(IDLE_COOLDOWN_MIN, IDLE_COOLDOWN_MAX)
@@ -53,6 +56,8 @@ func goto_idle_mode():
 func find_teleport_pos() -> Vector2:
 	var coords: Array[Vector2i] = []
 	for coord in map.get_used_cells(0):
+		if map.map_to_local(coord).distance_to(player.position) <= MIN_DISTANCE_TO_PLAYER:
+			continue
 		var cell = map.get_cell_tile_data(0, coord)
 		if cell == null or cell.get_collision_polygons_count(0) == 0: continue
 		coord = map.get_neighbor_cell(coord, TileSet.CELL_NEIGHBOR_TOP_SIDE)
@@ -60,6 +65,7 @@ func find_teleport_pos() -> Vector2:
 		if top_tile != null and top_tile.get_collision_polygons_count(0): continue
 		if map.get_cell_tile_data(1, coord) != null: continue # spawn blocker
 		coords.append(coord)
+	if not coords: return Vector2(INF, INF)
 	var coord := coords[randi_range(0, coords.size() - 1)]
 	return map.map_to_local(coord)
 
