1178640f76e51b49484a91c3a5a9f8d562e230e1
EliasSchaut <eschaut@outlook.de>
2024-04-16 02:52:41 +0200

    room tweeking

--- src/scripts/spell_items/magic_boost.gd
+++ src/scripts/spell_items/magic_boost.gd
@@ -4,7 +4,7 @@ func _init():
 	spell = SpellBook.Spells.MAGIC_BOOST
 	type = SpellBook.SpellType.PASSIVE
 	rank = SpellBook.SpellRank.COMMON
-	title = "Macic Boost"
+	title = "Magic Boost"
 	desc = "Passive: Increased damage of attacks for 10s"
 	cast = [2, 0, 3]
 	duration = 10.0
